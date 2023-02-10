b7a672068484d43c3b5ad584c34f8284c5e22193
E03\User <musni.danisidrei@auf.edu.ph>
2023-02-10 13:49:04 +0800

    FirstCommit

---
+++ PDC03_MODULE2test/PDC03_MODULE2test/activity2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE2test
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity2 : ContentPage
+    {
+        public activity2()
+        {
+            InitializeComponent();
+        }
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valuelabel.Text = args.NewValue.ToString("F3");
+        }
+    }
+}
